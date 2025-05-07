559d9ce7963c9e69de339dc3ab47a815dc2e9405
NamanSaxena2310git config --global user.name NamanSaxena2310git config --global user.name NamanSaxena2310 <namansaxena69@gmail.com>
2025-05-07 19:04:44 -0400

    product apis for admin side and routes created

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import  'dotenv/config';
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/userRoutes.js';
+import productRouter from './routes/productRoute.js';
 
 //App configuration
 const app = express()
@@ -17,6 +18,8 @@ app.use(cors())
 
 //Api endpoints
 app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
+
 
 app.listen(port,()=>{
   console.log("App running on port " , port)
