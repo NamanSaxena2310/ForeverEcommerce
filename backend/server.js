4dca7b1c55a04ec07bde8e9ded3b2cc559c4201a
NamanSaxena2310git config --global user.name NamanSaxena2310git config --global user.name NamanSaxena2310 <namansaxena69@gmail.com>
2025-05-09 15:53:08 -0400

    user cart and authentication implemented

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/userRoutes.js';
 import productRouter from './routes/productRoute.js';
+import cartRouter from './routes/cartRoute.js';
 
 //App configuration
 const app = express()
@@ -19,7 +20,7 @@ app.use(cors())
 //Api endpoints
 app.use('/api/user', userRouter)
 app.use('/api/product', productRouter)
-
+app.use('/api/cart',cartRouter)
 
 app.listen(port,()=>{
   console.log("App running on port " , port)
