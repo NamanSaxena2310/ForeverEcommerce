c029746a24329aa0084b7c3cca39b5b607fd92ce
NamanSaxena2310git config --global user.name NamanSaxena2310git config --global user.name NamanSaxena2310 <namansaxena69@gmail.com>
2025-05-10 18:34:55 -0400

    orders checkout api implemented

---
+++ backend/routes/oderRoute.js
@@ -0,0 +1,28 @@
+import express from "express";
+import {
+  placeOrder,
+  placeOrderRazorPay,
+  placeOrderStripe,
+  allOrders,
+  userOrders,
+  updateStatus,
+} from "../controllers/orderController.js";
+import adminAuth from "../middleware/adminAuth.js";
+import authUser from "../middleware/auth.js";
+
+const orderRouter = express.Router()
+
+//Admin Routes
+orderRouter.post('/list',adminAuth ,allOrders)
+orderRouter.post('/status',adminAuth ,updateStatus)
+
+//User Payment Routes
+orderRouter.post('/place',authUser ,placeOrder)
+orderRouter.post('/stripe',authUser ,placeOrderStripe)
+orderRouter.post('/razorpay',authUser ,placeOrderRazorPay)
+
+//User Routes
+orderRouter.post('/userorders',authUser,userOrders)
+
+
+export default orderRouter
